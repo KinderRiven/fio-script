fc699d04d223ac74c2f97018f0cc0369001e6bac
K <1339764596@qq.com>
2020-07-12 23:57:36 +0800

    push

---
+++ seqwrite.sh
@@ -0,0 +1,6 @@
+DATASET=" -size=10G -numjobs=4 -bs=4K"
+LOG=" -log_avg_msec=1000 -write_bw_log=mlog -write_iops_log=mlog -write_lat_log=mlog"
+IO=" -thread --direct=1 -ioengine=libaio"
+TIME=" -runtime=30 -time_based"
+
+echo $DATASET
