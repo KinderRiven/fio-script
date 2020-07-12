3766804c56a2a0148b12cf148820f1ea8d848773
K <1339764596@qq.com>
2020-07-12 21:10:17 +0800

    push

--- example_log.sh
+++ example_log.sh
@@ -4,6 +4,10 @@ direct=1
 ioengine=libaio 
 group_reporting
 
+# 记录IO -> 往文件的第[i]行，写了j大小的数据
+# write_iolog=[]
+# read_iolog=[]
+
 [write_job]
 size=50G
 numjobs=1
@@ -11,6 +15,7 @@ bs=4K
 rw=write
 runtime=30
 time_based
+
 log_avg_msec=1000
 write_bw_log=write_job
 write_iops_log=write_job
