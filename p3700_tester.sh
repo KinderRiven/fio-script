f36265cf6390c6693092c433536836e990f4518e
K <1339764596@qq.com>
2020-07-13 00:15:27 +0800

    push

--- p3700_tester.sh
+++ p3700_tester.sh
@@ -1,7 +1,7 @@
-DATASET="-name=file -size=10G -numjobs=4 -bs=4K"
-LOG=" -log_avg_msec=1000 -write_bw_log=mlog -write_iops_log=mlog -write_lat_log=mlog"
+DATASET="-name=file -size=50G -numjobs=4 -bs=16K"
+LOG=" -log_avg_msec=5000 -write_bw_log=mlog -write_iops_log=mlog -write_lat_log=mlog"
 IO=" -thread --direct=1 -ioengine=libaio"
-TIME=" -runtime=30 -time_based"
+TIME=" -runtime=3600 -time_based"
 
 echo $DATASET
 echo $IO
