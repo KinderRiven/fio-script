7762a515f038dbd2dc3d1b2be699fbbadd5dcbd0
K <1339764596@qq.com>
2020-07-12 19:38:56 +0800

    push

--- example_log.sh
+++ example_log.sh
@@ -3,15 +3,15 @@ thread
 direct=1
 ioengine=libaio 
 group_reporting
-log_avg_msec=1000
-write_bw_log=bw
-write_iops_log=iops
-write_lat_log=lat
 
 [write_job]
 size=50G
 numjobs=1
 bs=4K
 rw=write
-# runtime=30
-# time_based
+runtime=30
+time_based
+log_avg_msec=1000
+write_bw_log=write_job
+write_iops_log=write_job
+write_lat_log=write_job
