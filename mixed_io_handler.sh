38945c05e598907b9f994897782aacafc9d5dd7c
K <1339764596@qq.com>
2020-07-13 23:32:24 +0800

    push

--- mixed_io_handler.sh
+++ mixed_io_handler.sh
@@ -12,7 +12,7 @@ mv *.log p3700/readwrite
 
 # randomread
 echo 'P3700_randrws...'
-fio -group_reporting readwrite_test.fio > p3700_randread.log
+fio -group_reporting randreadwrite_test.fio > p3700_randread.log
 ./fio-fio-3.5/tools/fio_generate_plots mlog
 mkdir -p p3700/randrw
 mv *.svg p3700/randrw
@@ -32,7 +32,7 @@ mv *.log p4800/seqwrite
 
 # randomwrite
 echo 'P4800_readwrite...'
-fio -directory=/home/hanshukai/ssd2 -group_reporting readwrite_test.fio > p4800_randwrite.log
+fio -directory=/home/hanshukai/ssd2 -group_reporting randreadwrite_test.fio > p4800_randwrite.log
 ./fio-fio-3.5/tools/fio_generate_plots mlog
 mkdir -p p4800/randwrite
 mv *.svg p4800/randwrite
